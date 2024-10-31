68fa8fc4e72b5df96c0efed94e78d6eb3358d9a2
gianng25 <ducgiang2505@gmail.com>
2024-10-31 21:42:08 +0700

    Update LoadSave.java

--- src/main/java/utilz/LoadSave.java
+++ src/main/java/utilz/LoadSave.java
@@ -19,6 +19,10 @@ public class LoadSave {
 	public static final String GOKU_RUN = "res/walk.png";
 	public static final String MENU_BUTTONS = "res/button_atlas.png";
 	public static final String MENU_BACKGROUND = "res/menu_background.png";
+	public static final String PAUSE_BACKGROUND = "res/pause_menu.png";
+	public static final String SOUND_BUTTONS = "res/sound_button.png";
+	public static final String URM_BUTTONS = "res/urm_buttons.png";
+	public static final String VOLUME_BUTTONS = "res/volume_buttons.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage image = null;
